e31e2a78492c89a80f9756e5c5b8e94e4d895114
Sasidharan V <sasidharanv@Mac.domain.name>
2026-01-09 23:41:23 +0530

    Given a fresh look to the app

--- index.js
+++ index.js
@@ -8,29 +8,36 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-var blogsArray = new Array();
+// Using 'let' or 'const' is standard for 2026
+let blogsArray = []; 
 
 app.get("/", (req, res) => {
-  res.render("home.ejs");
+  // Pass the blogs to home if you want them displayed there too
+  res.render("home.ejs", { blog: blogsArray });
 });
 
-app.post("/", (req, res) => {
-  res.render("home.ejs");
-});
-
-app.get("/about", (req, res) => {
-  res.render("about.ejs", { blog: blogsArray });
-});
-
+// GET route for blogs: This is the ONLY place that should render the list
 app.get("/blogs", (req, res) => {
   res.render("blogs.ejs", { blog: blogsArray });
 });
 
+// POST route for blogs: This now REDIRECTS to prevent duplicates on refresh
 app.post("/blogs", (req, res) => {
-  // var blogObjects = req.body;
+  // Push the data to your array
   blogsArray.push(req.body);
-  console.log(blogsArray);
-  res.render("blogs.ejs", { blog: blogsArray });
+  
+  console.log("New blog added. Total blogs:", blogsArray.length);
+
+  /* 
+     CRITICAL CHANGE: 
+     Instead of res.render("blogs.ejs"), we use res.redirect("/blogs").
+     This clears the form data from the browser memory. 
+  */
+  res.redirect("/blogs"); 
+});
+
+app.get("/about", (req, res) => {
+  res.render("about.ejs");
 });
 
 app.get("/contact", (req, res) => {
@@ -41,11 +48,18 @@ app.get("/create", (req, res) => {
   res.render("create.ejs");
 });
 
+// Post/Redirect/Get pattern applied here too
 app.post("/submit", (req, res) => {
   const name = req.body["user"];
+  // If result.ejs is just a confirmation, this is fine, 
+  // but if it's a page you might refresh, consider redirecting to a GET /result
   res.render("result.ejs", { userName: name });
 });
 
+app.post("/", (req, res) => {
+  res.redirect("/"); 
+});
+
 app.listen(port, () => {
-  console.log(`server started running at post ${port}`);
+  console.log(`Server started running at http://localhost:${port}`);
 });
